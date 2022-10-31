acc43e6d43e4d8ec6565061c36e1f8a5382309c0
QB <54128450+kyuuube@users.noreply.github.com>
2022-10-31 17:33:56 +0800

    update

--- genApiTSFile.js
+++ genApiTSFile.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const https = require('https');
 const template = require('art-template');
 const fs = require('fs');
 const url = 'https://rosetta-latte.art/swagger/v2/api-docs';
@@ -8,33 +8,24 @@ let paths = null;
 let pathList = [];
 
 (async () => {
-    http.get(url, res => {
+    https.get(url, res => {
         let data = '';
         res.on('data', chunk => {
             data += chunk;
         });
         res.on('end', () => {
             data = JSON.parse(data);
-            basePath = data.basePath;
+            basePath = data.basePath === '/' ? '' : data.basePath;
             paths = data.paths;
-            if (Object.prototype.toString.call(paths) === '[object Object]') {
-                console.log("paths is an Object");
-            } else if (Array.isArray(paths)) {
-                console.log("paths is an Array");
-            } else if (paths instanceof Map) {
-                console.log("is map");
-            } else {
-                // is not [Object Object] , but [object Object]
-                console.log(Object.prototype.toString.call(paths) === '[Object Object]');
-            }
             pathList = Object.keys(paths).map(key => ({...paramsFlat(paths[key]), url: basePath + key}))
-            fileData = template(__dirname + '/template/tpl-apiFile.art', {
-                list: pathList
-            });
-            fileData = replaceBlank(fileData)
-            fs.writeFile(__dirname + `/dist/${basePath}Api.js`, fileData, function (err) {
-                if (err) return console.log(err);
-                console.log('finish');})
+            console.log(pathList)
+            // fileData = template(__dirname + '/template/tpl-apiFile.art', {
+            //     list: pathList
+            // });
+            // fileData = replaceBlank(fileData)
+            // fs.writeFile(__dirname + `/dist/${basePath}Api.js`, fileData, function (err) {
+            //     if (err) return console.log(err);
+            //     console.log('finish');})
         });
     }).on('error', err => {
         console.log(err.message);
